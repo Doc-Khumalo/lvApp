0073f01d0bcb4512763a21e30a830bc1f5c18c6f
Leslie Khumalo <leslie.khumalo@icloud.com>
2019-06-21 09:48:27 +0100

    Updates

--- src/modules/type.js
+++
@@ -1,5 +0,0 @@
-export const GET_DATA = 'lvApp/GET_DATA';
-export const GET_SCORE = 'lvApp/GET_SCORE';
-export const GET_PAGE_INFO = 'lvApp/GET_PAGE_INFO';
-export const GET_NEW_DIAGNOSIS = 'lvApp/GET_NEW_DIAGNOSIS';
-export const GET_DIAGNOSIS_INFO = 'lvApp/GET_DIAGNOSIS_INFO';
